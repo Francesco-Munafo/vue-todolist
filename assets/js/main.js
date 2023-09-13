5a406c5f031714b70e7442a1738cc260e18eb086
Francesco-Munafo <acquamarea1@gmail.com>
2023-09-13 17:44:36 +0200

    Add html content and js scripts for milestones 1,2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /*
 
-Descrizione: Rifare l’esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+Descrizione: Rifare l’esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 text, una stringa che indica il testo del todo
 done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
@@ -10,7 +11,8 @@ MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di e
 
 MILESTONE 3 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
-Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
 */
 
@@ -20,7 +22,52 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+
+        pageLogo: './assets/img/Logo.png',
+        newTask: '',
+
+
+        toDoList:[
+        {
+          text: 'Fare la spesa',
+          done: true
+        },
+        {
+          text: 'Fare la spesa' ,
+          done: false
+        },
+        {
+          text: 'Fare la spesa' ,
+          done: false
+        },
+        {
+          text: 'Fare la spesa' ,
+          done: false
+        },
+        {
+          text: 'Fare la spesa' ,
+          done: false
+        }
+      ] 
+       
+      }
+    },
+    methods: {
+
+      taskDone(index){
+        if (this.toDoList[index].done) {
+
+          this.toDoList[index].done = true
+
+        } else {
+
+          this.toDoList[index].done = false
+
+        }
+        
+      },
+      removeTask(index){
+        this.toDoList.splice(index, 1)
       }
     }
   }).mount('#app')
